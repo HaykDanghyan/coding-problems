98186c4f8023cc24c4dd284c140907cbf21dcc37
HaykDanghyan <haykhermine07@gmail.com>
2022-09-04 14:49:21 +0400

    set matrix zeroes, looks like the same problem in LeetCode

---
+++ Arrays and Strings/1.9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+bool stringRotation(std::string& s1, std::string& s2) {
+    auto x = s1.length();
+    while (x--) {
+        if (s1 == s2) {
+            return true;
+        }
+        s1 = s1.substr(1) + s1[0];
+    }
+    return false;
+}
+
+int main() {
+    std::string s1 = "waterbottle";
+    std::string s2 = "erbottlewat";
+    std::cout << std::boolalpha << stringRotation(s1, s2) << std::endl;
+}
